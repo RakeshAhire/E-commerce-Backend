6902032ffdf6de3cbb85e2ab478c66b2fb6acc56
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-18 21:45:40 +0530

    added

--- Routes/Vendor.Routes.js
+++ Routes/Vendor.Routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { VendorModel } = require("../Model/Vendor.model");
+const { VendorModel } = require("../Model/vendor.model");
 
 const saltRounds = +process.env.saltRounds;
 
