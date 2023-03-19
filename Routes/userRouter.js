1049755ac0e09101f017fe957cdfd04a07bb5f56
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-19 15:52:26 +0530

    added

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const _ = require("lodash");
 const axios = require("axios");
@@ -7,76 +7,78 @@ const otpGenerator = require("otp-generator");
 const User = require("../Model/user.model");
 const { Otp } = require("../Model/otpModel");
 
-const router = express.Router();
-
-const SMS_GATEWAY_API_KEY = process.env.SMS_GATEWAY_API_KEY;
-const OTP_LENGTH = 6;
-
 router.post("/signup", async (req, res) => {
-  try {
-    const { number } = req.body;
+  const user = await User.findOne({
+    number: req.body.number,
+    email: req.body.email
+  });
+  if (user) {
+    return res.status(400).send("User already Registred!");
+  }
 
-    const user = await User.findOne({ number });
-    if (user) {
-      return res.status(400).send("User already registered");
-    }
+  const OTP = otpGenerator.generate(6, {
+    digits: true,
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
 
-    const OTP = otpGenerator.generate(OTP_LENGTH, {
-      digits: true,
-      lowerCaseAlphabets: false,
-      upperCaseAlphabets: false,
-      specialChars: false,
+  const number = req.body.number;
+  const name = req.body.name;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  const greenwebsms = new URLSearchParams();
+  greenwebsms.append("token", "05fa33c4cb50c35f4a258e85ccf50509");
+  greenwebsms.append("to", `+${number}`);
+  greenwebsms.append("message", `Verification Code ${OTP}`);
+  axios
+    .post("http://api.greenweb.com.bd/api.php", greenwebsms)
+    .then((response) => {
+      console.log(response.data);
     });
 
-    const greenwebsms = new URLSearchParams();
-    greenwebsms.append("token", SMS_GATEWAY_API_KEY);
-    greenwebsms.append("to", `+${number}`);
-    greenwebsms.append("message", `Verification Code ${OTP}`);
+  const otp = new Otp({ number: number,
+    name:name,
+    password:password,
+    email:email,
+    otp: OTP });
 
-    await axios.post("http://api.greenweb.com.bd/api.php", greenwebsms);
+  const salt = await bcrypt.genSalt(10);
+  otp.otp = await bcrypt.hash(otp.otp, salt);
+  otp.password=await bcrypt.hash(otp.password, salt);
 
-    const otp = new Otp({ number, otp: OTP });
-
-    const salt = await bcrypt.genSalt(10);
-    otp.otp = await bcrypt.hash(otp.otp, salt);
-
-    const result = await otp.save();
-    return res.status(200).send({ status: "success", otp: OTP });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Internal Server Error");
-  }
+  const result = await otp.save();
+  return res.status(200).send({ status: "success", otp: OTP });
 });
 
 router.post("/signup/verify", async (req, res) => {
-  try {
-    const { number, otp } = req.body;
+  const otpHolder = await Otp.find({
+    number: req.body.number,
+    email: req.body.email,
+  });
+  if (otpHolder.length === 0) {
+    return res.status(404).send("You have used Expired OTP!");
+  }
 
-    const otpHolder = await Otp.find({ number });
-    if (otpHolder.length === 0) {
-      return res.status(404).send("You have used an expired OTP");
-    }
+  const rightOtpFind = otpHolder[otpHolder.length - 1];
+  const validUser = await bcrypt.compare(req.body.otp, rightOtpFind.otp);
 
-    const rightOtpFind = otpHolder[otpHolder.length - 1];
-    const validUser = await bcrypt.compare(otp, rightOtpFind.otp);
+  if (rightOtpFind.number === req.body.number && validUser) {
+    const user = new User(_.pick(req.body, ["number"]));
+    const token = user.generateJWT();
+    const result = await user.save();
+    const OTPDelete = await Otp.deleteMany({
+      number: rightOtpFind.number,
+    });
 
-    if (rightOtpFind.number === number && validUser) {
-      const user = new User(_.pick(req.body, ["number"]));
-      const token = user.generateJWT();
-      const result = await user.save();
-      const OTPDelete = await Otp.deleteMany({ number: rightOtpFind.number });
-
-      return res.status(200).send({
-        message: "User Registration Successful",
-        token,
-        data: result,
-      });
-    } else {
-      return res.status(400).send("Invalid OTP");
-    }
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Internal Server Error");
+    return res.status(200).send({
+      message: "User login Successfull!",
+      token: token,
+      data: result,
+    });
+  } else {
+    return res.status(400).send("Your OTP was wrong");
   }
 });
 
