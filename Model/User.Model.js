73507cbce5ccef8f30784ad3a7f168028a124c83
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-19 16:15:58 +0530

    added

--- Model/User.Model.js
+++ Model/User.Model.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 let jwt = require("jsonwebtoken");
 const key = "774bgtumnhnhydscffsr";
-const userSchema = new mongoose.Schema(
-  {
-    name: String,
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+    },
     email: {
       type: String,
     },
@@ -19,18 +22,18 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.methods.generateJWT = function () {
-  const token = jwt.sign(
-    {
-      _id: this._id,
-      number: this.number,
-    },
-    ke,
-    { expiresIn: "7d" }
-  );
-  return token;
-};
+// userSchema.methods.generateJWT = function () {
+//   const token = jwt.sign(
+//     {
+//       _id: this._id,
+//       number: this.number,
+//     },
+//     ke,
+//     { expiresIn: "7d" }
+//   );
+//   return token;
+// };
 
-const {userModel} = mongoose.model("users", userSchema);
+const userModel = mongoose.model("users", userSchema);
 
 module.exports = {userModel};
