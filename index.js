e78cb997d4551589864bb11ad806251b9d6d80cd
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-21 16:40:15 +0530

    added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.use("/contact",ContactRoutes)
+// app.use("/contact",ContactRoutes)
 
 app.use("/otp", require("./Routes/otp.routes"));
 app.use("/email", EmailRoutes);
