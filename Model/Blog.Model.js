f962d63faa15335b9e5c997208f240e927184df3
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-26 15:50:22 +0530

    added

--- Model/Blog.Model.js
+++ Model/Blog.Model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const BlogSchema = new mongoose.Schema(
   {
     title: { type: String },
-    image: [{ type: String }],
+    image: { type: String },
     date:{ type: Number },
     month:{ type: String },
     description: { type: String },
