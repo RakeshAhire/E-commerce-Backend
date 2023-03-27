19dfc8d360b90672ac30eed07306ff4fcdeaab79
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-27 18:47:14 +0530

    added

--- Model/Cart.Model.js
+++ Model/Cart.Model.js
@@ -8,6 +8,7 @@ const cartSchema = new mongoose.Schema(
     color: { type: String, required: true },
     size: { type: String, required: true },
     shipping: { type: String, required: true },
+    
     quantity: { type: Number, default: 1, min: 1 },
     productId: {
       type: mongoose.Schema.Types.ObjectId,
